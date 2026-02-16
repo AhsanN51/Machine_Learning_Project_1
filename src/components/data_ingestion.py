92c60e65bf280efc73a3d5c82d496da5a71c85f2
AhsanN51 <muhammadahsan8013@gmail.com>
2026-02-16 17:33:32 +0530

    '11'

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -15,9 +15,9 @@ from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join("model_data", "train.csv")
-    test_data_path: str = os.path.join("model_data", "test.csv")
-    raw_data_path: str = os.path.join("model_data", "raw.csv")
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "raw.csv")
 
 
 class DataIngestion:
